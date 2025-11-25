d5220bf00e862de61eb3868934f7596a3cfc05d1
PRASHANT TYAGI <prashanttyagi7589@gmail.com>
2025-11-25 13:29:13 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { registerUser } from "../api/userApi";
+import { registerUser } from "../api/userapi";
 import { useNavigate } from "react-router-dom";
 import {
   Box,
