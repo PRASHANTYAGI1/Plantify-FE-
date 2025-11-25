d69948ea0104f3d110ed7f1302765b981ea44f5b
PRASHANT TYAGI <prashanttyagi7589@gmail.com>
2025-11-25 13:18:53 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from "react";
-import { getUserProfile } from "../api/userApi";
+import { getUserProfile } from "../api/userapi";
 
 export const AuthContext = createContext();
 
